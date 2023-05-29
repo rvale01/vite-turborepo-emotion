6415368428369c750d0290112a6a4248303f32b3
valentina.ronchi@docplanner.com <49074392+rvale01@users.noreply.github.com>
2023-05-29 18:26:02 +0100

    fixes

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -3,18 +3,6 @@ import { ThemeProvider, useTheme } from '@emotion/react';
 
 const theme = {
   main: "#3D83DF",
-  black: "#000000",
-  blue: "#3D83DF",
-  blueLight: "#EEF4FD",
-  red: "#E74C3C",
-  white: "#FFFFFF",
-  gray100: "#F7F9FA",
-  gray200: "#EDEFF2",
-  gray300: "#DCDFE3",
-  gray500: "#98A1AB",
-  gray600: "#627282",
-  gray900: "#1B2734",
-  green: "#3fd071"
 }
 function Component() {
   const theme = useTheme();
