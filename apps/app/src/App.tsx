b6d7359fc66fd711e7249a911e5bf6d8d7fda60e
valentina.ronchi@docplanner.com <49074392+rvale01@users.noreply.github.com>
2023-05-31 16:27:52 +0100

    heu

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -16,16 +16,9 @@ function Component() {
 function App() {
   const iframeRef = useRef(null);
 
-  //TODO: postMessage => provare con * => controllare se in termini di sicurezza e' meno sicuro o meno.
-  const sendMessage = () => {
-    // @ts-ignore
-    iframeRef.current.contentWindow.postMessage('ciao ciao bambinaaaaa lalala....', 'http://localhost/mop/index.php?dbName=tt_istanza_1#login');
-  }
   return (
     <ThemeProvider theme={theme}>
       <Component />
-      <button onClick={sendMessage}>Send Message</button>
-      <iframe ref={iframeRef} src="http://localhost/mop/index.php?dbName=tt_istanza_1#login" title="Iframe Example"></iframe>
     </ThemeProvider>
   );
 }
